3742157e784c728aeb0f35121e2275d8532bff19
ashyapeachie <128183143+ashyapeachie@users.noreply.github.com>
2026-06-26 00:48:58 -0400

    code for matcha.js
    making Matcha as an object

---
+++ js/matcha.js
@@ -0,0 +1,17 @@
+class Matcha {
+    constructor(
+        brand,
+        grade,
+        rating,
+        price,
+        notes,
+        favorites
+    ){
+        this.brand = brand;
+        this.grade = grade;
+        this.rathing = rating;
+        this.price = price;
+        this.notes = notes;
+        this.favorites = favorites;
+    }
+}
